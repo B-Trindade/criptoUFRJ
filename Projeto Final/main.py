e87319a3c49cba70cbe51bec72b5e8334d377680
Breno Trindade <bttostes@gmail.com>
2017-06-29 05:51:24 -0300

    lots of changes

--- Projeto Final/main.py
+++ Projeto Final/main.py
@@ -14,6 +14,23 @@ from hashlib import sha224
 #texto, por exemplo, podemos calcular o seu hash com a seguinte linha:
 #h = sha224(texto).hexdigest()
 
+#Fazer codigo capaz de ler valores de arquivos.
+        # file_ = open("filename", "r") #mesma coisa de C aqui
+                # r para read
+                # w para write
+                # a para append
+                # r+ para ler e escrever
+        # para fechar o arquivo, usa-se:
+        # file_.close() #so use quando tiver acabado de usar o arquivo completamente
+        # uma vez fechado, nao eh possivel abrir de novo durante o programa
+
+        # TUDO QUE VOCE PRECISA SABER PARA MEXER COM ARQUIVO EM PYTHON #
+            # utilizando o mesmo file_ como la de cima, use:
+            # reading = file_.read() para colocar todo o conteudo do arquivo na string reading
+            # reading = file_.read(n) para ler n caracteres do arquivo e armazenar em reading
+            # reading = file_.readline(n) para ler o que ta escrito na linha n do arquivo e armazena em reading
+
+            # writing = file_.write("o que vc quiser escrever") para escrever no arquivo - uma unica linha
 
 def menu_principal():
     '''Funcao apresenta a possibilidade de escolher entre
@@ -122,54 +139,6 @@ def encriptacao_pura_menu():
 
     pass
 
-#Missing
-def get_key():
-    '''Recebe, pelo terminal ou por um arquivo, a chave publica que sera usada.'''
-
-    print("""Deseja informar a chave publica no formato hexadecimal pelo terminal
-             ou ler de um arquivo que a contenha?""")
-
-    print("1) Digitar no terminal")
-    print("2) Ler de um arquivo")
-    print(" ")
-
-    #Obtendo a resposta do usuario:
-    modus = input()
-
-    if (modus == 1):
-        #Adequar para que key receba o valor em hexadecimal
-        print("Insira o valor de 'n'.")
-        key = input()
-        #O valor de entrada eh hexadecimal, para transforma-lo em decimal, use:
-        # i = (key, 16)
-    else:
-        print("Insira o nome do arquivo para leitura da chave.")
-        nome_arq_ChavePublica = raw_input()
-
-        with open(nome_arq_ChavePublica, 'r') as arq_ChavePublica:
-            arq_ChavePublica.read()
-
-
-        #Fazer codigo capaz de ler valores de arquivos.
-        # file_ = open("filename", "r") #mesma coisa de C aqui
-                # r para read
-                # w para write
-                # a para append
-                # r+ para ler e escrever
-        # para fechar o arquivo, usa-se:
-        # file_.close() #so use quando tiver acabado de usar o arquivo completamente
-        # uma vez fechado, nao eh possivel abrir de novo durante o programa
-
-        # TUDO QUE VOCE PRECISA SABER PARA MEXER COM ARQUIVO EM PYTHON #
-            # utilizando o mesmo file_ como la de cima, use:
-            # reading = file_.read() para colocar todo o conteudo do arquivo na string reading
-            # reading = file_.read(n) para ler n caracteres do arquivo e armazenar em reading
-            # reading = file_.readline(n) para ler o que ta escrito na linha n do arquivo e armazena em reading
-
-            # writing = file_.write("o que vc quiser escrever") para escrever no arquivo - uma unica linha
-
-    return key
-
 def decriptacao_pura_menu():
     '''A funcao oferece a escolha para decriptacao por RSA ou El Gamal. Obtem o nome
     do arquivo a ser decriptado, alem de receber a chave privada de encriptacao (lida
@@ -186,7 +155,7 @@ def decriptacao_pura_menu():
 
     #Obtendo o nome do arquivo a ser encriptado:
     print("Insira, agora, o nome do arquivo a ser decriptado: ")
-    arquivo_fonte = input()
+    arquivo_fonte = raw_input()
     print(" ")
 
     #Verificando a escolha do usuario:
@@ -204,12 +173,11 @@ def assinatura_digital_pura_menu():
 
     print(" ")
     print("Insira o nome do arquivo cujo conteudo sera assinado.")
-    arquivo_fonte = input()
+    arquivo_fonte = raw_input()
     print(" ")
-    chave_privada_assinatura = get_key()
 
     #Evoca a funcao que de fato executa o algoritmo de assinatura digital
-    assinatura_digital_pura(chave_privada_assinatura)
+    assinatura_digital_pura(arquivo_fonte)
 
     pass
 
@@ -244,7 +212,7 @@ def comb_decriptacao_verificacao_menu():
     pass
 
 #
-#-----------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 #
 
 #Missing
@@ -262,9 +230,9 @@ def chaves_RSA():
     #Define dois numeros que certamente sao compostos para que
     #eles caiam no caso de randomizacao para numeros de 128 bits
 
-    while (miller_rabin(p,2) is True):
+    while (miller_rabin(p,2,2) is True):
         p = random.getrandbits(128) #Obtem um valor de 128 bits inteiro
-    while (miller_rabin(q,2) is True):
+    while (miller_rabin(q,2,2) is True):
         q = random.getrandbits(128) #Obtem um valor de 128 bits inteiro
 
     # Nestes casos foi utilizado o algoritmo de
@@ -345,9 +313,12 @@ def chaves_ElGamal():
     c,a = 0,0
     g = 2
 
-    while ((p.bit_length() != 256 or q.bit_length() != 255) or (miller_rabin(q,2) is False or miller_rabin(p,2) is False)):
+    while ((p.bit_length() != 256 or q.bit_length() != 255) or (miller_rabin(q,2,2) is False or miller_rabin(p,2,2) is False)):
         q = random.getrandbits(255)
         p = 2*q + 1
+
+    #Calculamos "c" de forma que "g" elevado a "a" seja congruente a "c" mod p e c != 1
+    #Assim, temos um "g" gerador de U(p) e um "p" primo de forma que U(p) e ciclico.
     while (potenciamodular(g, a , p) == 1):
         g += 1
 
@@ -359,40 +330,191 @@ def chaves_ElGamal():
     p = converte_para_hexadecimal(p)
     g = converte_para_hexadecimal(g)
 
-    #----------------------------------------------------------------------------------------------------------
-    pass
+    #----------- Fim do algoritmo para a criacao de chaves El Gamal ------------
+
+    print(" ")
+    print("1) Imprimir os valores gerados no terminal.")
+    print("2) Armazenar os valores gerados em dois arquivos (um para cada par de chaves).")
+    print(" ")
+
+    #Recebendo o comando do usuario.
+    modus = input()
+
+    #Verificando a escolha do usuario:
+    if (modus == 1):
+        print("Chave Publica: p = " + str(p) + "; g = " + str(g) + "; c = " + str(c))
+        print("Chave Privada: a = " + str(a))
+        print(" ")
+
+    elif (modus == 2):
+
+        print("Informe o nome do arquivo que ira conter as chaves publicas.")
+        nome_arq_ChavePublica = raw_input()
+        print(" ")
+
+        print("Informe o nome do arquivo que ira conter as chaves privadas.")
+        nome_arq_ChavePrivada = raw_input()
+        print(" ")
+
+        print("""Os valores serao armazenados na seguinte ordem: 'p', 'g' e 'c'.
+             Seguidos de 'a' no arquivo seguinte.""")
+
+        with open(nome_arq_ChavePublica, 'w') as arq_ChavePublica:
+            arq_ChavePublica.write(str(p) + '\n')
+            arq_ChavePublica.write(str(g) + '\n')
+            arq_ChavePublica.write(str(c) + '\n')
+        arq_ChavePublica.closed
+
+        with open(nome_arq_ChavePrivada, 'w') as arq_ChavePrivada:
+            arq_ChavePrivada.write("a = " + str(a) + '\n')
+        arq_ChavePrivada.closed
+
+        print(" ")
+        print("Arquivos produzidos com sucesso!")
+        print("Redirecionando para o menu principal...")
+
+    #-----------------------Fim das opcoes interativas--------------------------
+        return menu_principal
 
 #Missing
 def encriptacao_RSA(arquivo):
     '''.'''
-    public_key = get_key()
+    n_key,e_key = get_keyRSA()
 
-    pass
+    with open(arquivo, 'r') as arquivo_fonte:
+        mensagem = arquivo_fonte.read()
+    arquivo_fonte.closed
+
+    print("Insira o nome do arquivo encriptado.")
+    novo_arquivo = raw_input()
+    print(" ")
+
+    encript_arq = open(novo_arquivo, 'w')
+
+    while (i < len(mensagem)):
+        bloco = ord(mensagem[i])
+        bloco = potenciamodular(bloco, e_key, n_key)
+        encript_arq.write(str(bloco) + '\n')
+        
+        i += 1
+
+    encript_arq.close()
+
+    print("Sucesso! Arquivos encriptados.")
+    return menu_principal()
 
 #Missing
 def encriptacao_ElGamal(arquivo):
     '''.'''
-    public_key = get_key()
+    p_key,g_key,c_key = get_keyEG()
 
-    pass
+    with open(arquivo, 'r') as arquivo_fonte:
+        mensagem = arquivo_fonte.read()
+    arquivo_fonte.closed
+
+    print("Insira o nome do arquivo encriptado.")
+    novo_arquivo = raw_input()
+    print(" ")
+
+    encript_arq = open(novo_arquivo, 'w')
+
+    while (i < len(mensagem)):
+        bloco = ord(mensagem[i])
+        m = random.randint(2, p_key -2)
+        n = (bloco * potenciamodular(c_key, m, p_key)) % p_key
+        o = potenciamodular(g_key, m, p_key)
+        
+        encript_arq.write(str(n) + '\n')
+        encript_arq.write(str(o) + '\n')
+        
+        i += 1
+    encript_arq.close()
+
+    print("Sucesso! Arquivos encriptados.")
+    return menu_principal()
 
 #Missing
 def decriptacao_RSA(arquivo):
-    '''.'''
-    private_key = get_key()
+    '''Recebe um arquivo com conteudo encriptado por RSA'''
+    n_key,d_key = get_pkeyRSA()
 
-    pass
+    arquivo_fonte = open(arquivo, 'r') 
+
+    print("Insira o nome do arquivo decriptado.")
+    novo_arquivo = raw_input()
+    print(" ")
+
+    arq_drecriptado = open(novo_arquivo, 'w')
+
+    linha = arquivo_fonte.readline()
+    while(linha != ''):
+        bloco = linha
+        bloco = potenciamodular(int(bloco), d_key, n_key)
+        bloco = chr(bloco)
+
+        arq_drecriptado.write(bloco)
+        linha = arquivo_fonte.readline()
+
+    arquivo_fonte.close()
+    arq_drecriptado.close()
+    
+    return menu_principal()
 
 #Missing
 def decriptacao_ElGamal(arquivo):
     '''.'''
-    private_key = get_key()
+    p_key,g_key,a_key = get_pkeyEG()
 
-    pass
+    print("Insira o nome do arquivo decriptado.")
+    novo_arquivo = raw_input()
+    print(" ")
+
+    arquivo_fonte = open(arquivo, 'r')
+    arq_drecriptado = open(novo_arquivo, 'w')
+
+    linha_n = arquivo_fonte.readline()
+    linha_o = arquivo_fonte.readline()
+
+    while ((linha_n or linha_o) != ''):
+        linha_n = potenciamodular((int(linha_n)), p_key-1-a_key, p_key)
+        bloco = (linha_n * int(linha_o)) % p_key
+        bloco = chr(bloco)
+
+    arq_drecriptado.close()
+    arquivo_fonte.close()
+
+    return menu_principal()
 
 #Missing
-def assinatura_digital_pura(chave_privada_assinatura):
+def assinatura_digital_pura(arquivo):
     '''.'''
+    p_key,g_key,a_key = get_pkeyEG()
+
+    #Obtem o conteudo a ser assinado
+    arquivo_fonte = open(arquivo, 'r')
+
+    texto = ''
+    linha_texto = arquivo_fonte.readline()
+    while (linha_texto != ''):
+
+        for (i in linha_texto):
+            bloco = ord(i)
+            texto += str(bloco)
+
+        linha_texto = arquivo_fonte.readline()
+        
+    h = sha224(texto).hexdigest()
+
+    k = random.randint(1, p_key - 2)
+    while (euclidiano(k, p_key -1) is False):
+        k = random.randint(1, p_key - 2)
+
+    r = potenciamodular(g_key, k, p_key)
+
+    inverso_k = euclidiano_estendido(k, p_key-1)
+
+    s = (inverso_k * int(h - a_key * r)) % (p_key -1)
+    dec_embaralhada = int(h, 16)
 
     pass
 
@@ -406,6 +528,173 @@ def verificacao_assinatura_pura(chave_publica_verificacao):
 #Funcoes para resultados intermediarios e adequacao ao formato desejado.
 #Nao consituem funcoes chamadas diretamente por uma escolha do usuario,
 #entretanto, aparecem dentro de outras funcoes.(NAO PERTENCEM AS FUNCOES PREVIAS)
+
+def get_pkeyEG():
+    '''Recebe pelo terminal (em haxadecimal), ou por um arquivo, a chave privada a ser usada.'''
+
+    print("""Deseja informar a chave privada no formato hexadecimal pelo terminal
+             ou ler de um arquivo que a contenha?""")
+
+    print("1) Digitar no terminal")
+    print("2) Ler de um arquivo")
+    print(" ")
+
+    #Obtendo a resposta do usuario:
+    modus = input()
+
+    if (modus == 1):
+        print("Insira o valor de 'p'.")
+        p_Hkey = raw_input()
+        p_key = int(p_Hkey, 16)
+
+        print("Insira o valor de 'g'.")
+        g_Hkey = raw_input()
+        g_key = int(g_Hkey, 16)
+
+        print("Insira o valor de 'a'.")
+        a_Hkey = raw_input()
+        a_key = int(a_Hkey, 16)
+
+    else:
+        print("Insira o nome do arquivo para a leitura da chave")
+        nome_arq_ChavePrivada = raw_input()
+
+        with open(nome_arq_ChavePrivada, 'r') as arq_ChavePrivada:
+            p_Skey = arq_ChavePrivada.readline()
+            p_key = int(p_Skey[0:-1])
+
+            g_Skey = arq_ChavePrivada.readline()
+            g_key = int(g_Skey[0:-1])
+
+            a_Skey = arq_ChavePrivada.readline()
+            a_key = int(a_Skey[0:-1])
+
+        arq_ChavePrivada.closed
+
+    return p_key,g_key,a_key
+
+def get_pkeyRSA():
+    '''Recebe pelo terminal (em haxadecimal), ou por um arquivo, a chave privada a ser usada.'''
+
+    print("""Deseja informar a chave privada no formato hexadecimal pelo terminal
+             ou ler de um arquivo que a contenha?""")
+
+    print("1) Digitar no terminal")
+    print("2) Ler de um arquivo")
+    print(" ")
+
+    #Obtendo a resposta do usuario:
+    modus = input()
+
+    if (modus == 1):
+        print("Insira o valor de 'n'.")
+        n_Hkey = raw_input()
+        n_key = int(n_Hkey, 16)
+
+        print("Insira o valor de 'd'.")
+        d_Hkey = raw_input()
+        d_key = int(d_Hkey, 16)
+
+    else:
+        print("Insira o nome do arquivo para a leitura da chave")
+        nome_arq_ChavePrivada = raw_input()
+
+        with open(nome_arq_ChavePrivada, 'r') as arq_ChavePrivada:
+            n_Skey = arq_ChavePrivada.readline(0)
+            n_key = int(n_Skey[0:-1])
+
+            d_Skey = arq_ChavePrivada.readline(1)
+            d_key = int(d_Skey[0:-1])
+
+        arq_ChavePrivada.closed
+
+    return n_key,d_key
+
+#Missing
+def get_keyRSA():
+    '''Recebe, pelo terminal ou por um arquivo, a chave publica que sera usada.'''
+
+    print("""Deseja informar a chave publica no formato hexadecimal pelo terminal
+             ou ler de um arquivo que a contenha?""")
+
+    print("1) Digitar no terminal")
+    print("2) Ler de um arquivo")
+    print(" ")
+
+    #Obtendo a resposta do usuario:
+    modus = input()
+
+    if (modus == 1):
+        #Adequa para que key receba o valor em hexadecimal
+        print("Insira o valor de 'n'.")
+        n_Hkey = raw_input()
+        n_key = int(n_Hkey, 16)
+
+        print("Insira o valor de 'e'.")
+        e_Hkey = raw_input()
+        e_key = int(e_Hkey, 16)
+
+    else:
+        print("Insira o nome do arquivo para leitura da chave.")
+        nome_arq_ChavePublica = raw_input()
+
+        with open(nome_arq_ChavePublica, 'r') as arq_ChavePublica:
+            n_Skey = arq_ChavePublica.readline(0)
+            n_key = int(n_Skey[0:-1])
+
+
+            e_Skey = arq_ChavePublica.readline(1)
+            e_key = int(e_Skey[0:-1])
+
+        arq_ChavePublica.closed
+
+    return n_key,e_key
+
+def get_keyEG():
+    '''Recebe, pelo terminal ou por um arquivo, a chave publica que sera usada.'''
+
+    print("""Deseja informar a chave publica no formato hexadecimal pelo terminal
+             ou ler de um arquivo que a contenha?""")
+
+    print("1) Digitar no terminal")
+    print("2) Ler de um arquivo")
+    print(" ")
+
+    #Obtendo a resposta do usuario:
+    modus = input()
+
+    if (modus == 1):
+        #Adequa para que key receba o valor em hexadecimal
+        print("Insira o valor de 'p'.")
+        p_Hkey = raw_input()
+        p_key = int(p_Hkey, 16)
+
+        print("Insira o valor de 'g'.")
+        g_Hkey = raw_input()
+        g_key = int(g_Hkey, 16)
+
+        print("Insira o valor de 'c'.")
+        c_Hkey = raw_input()
+        c_key = int(c_Hkey, 16)
+
+    else:
+        print("Insira o nome do arquivo para leitura da chave.")
+        nome_arq_ChavePublica = raw_input()
+
+        with open(nome_arq_ChavePublica, 'r') as arq_ChavePublica:
+            p_Skey = arq_ChavePublica.readline(0)
+            p_key = int(p_Skey[0:-1])
+
+
+            g_Skey = arq_ChavePublica.readline(1)
+            g_key = int(g_Skey[0:-1])
+
+            c_Skey = arq_ChavePublica.readline(2)
+            c_key = int(c_Skey[0:-1])
+
+        arq_ChavePublica.closed
+
+    return p_key,g_key,c_key
 
 #Missing
 def converte_para_hexadecimal(x):
@@ -424,11 +713,12 @@ def converte_para_decimal(x):
 
 #Funcoes previamente utilizadas nos trabalhos semanais:
 
-def miller_rabin(n, b):
+def miller_rabin(n, b, first_base):
     '''.'''
 
     r,k = 1,0
-    q = (m - 1)
+    q = (n - 1)
+    last_b = b
 
     while (q % 2 == 0):
         q = q/2
@@ -440,22 +730,28 @@ def miller_rabin(n, b):
         i = q%2
         if (q == 0): break
         if (i == 1):
-            r = (r*a) % m
+            r = (r*b) % n
             q = (q-1) / 2
         else:
             q = q/2
-        a = (a*a) % m
+        b = (b*b) % n
 
     while (k > 0):
         k -= 1
 
-        if (r == m-1) or (k == (l-1) and r == 1):
-            return False
+        if (r == n-1) or (k == (l-1) and r == 1):
+            last_b += 1
+
+            if (first_base == last_b - 10):
+                return False
+            else:
+                miller_rabin(n, last_b, first_base)
+
         if (k == 0):
             return True
 
-        e = (e*2) % m
-        r = (r*r) % m
+        e = (e*2) % n
+        r = (r*r) % n
 
     pass
 
@@ -500,44 +796,18 @@ def euclidiano_estendido(a, b):
 #Missing description
 def modulo(n,m):
     '''.'''
-	resultado = 0
-	num = n
-	mod = m
-	if(num < 0):
+    resultado = 0
+    num = n
+    mod = m
+    if(num < 0):
 		num *= -1
 		if(num < mod):
 			resultado = mod - num
 		else:
 			resultado = num % mod
-	else:
+    else:
 		result = num % mod
-	return result
-
-#Missing description
-def inversomodular(a, mod):
-    modfixo = mod
-    x1, y = 1
-    x, y1 = 0
-    while a%mod > 0:
-        r = a%mod
-        q = a/mod
-        if r != 0:
-            a = mod
-            mod = r
-            xr = x1
-            x1 = x
-            x = xr-q*x
-            yr = y1
-            y1 = y
-            y = yr-q*y
-    if r == 1:
-        while x < 0:
-            i = 1
-            x = x + modfixo*i
-            i = i + 1
-        if x > modfixo:
-            x = x%modfixo
-    return x
+    return result
 
 #Missing description
 def potenciamodular(a, e, mod):
